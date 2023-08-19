d61b26b9af8897cf34fed4efd767d89d93a7aead
Sherzod <shortfilmuz@gmail.com>
2023-08-20 01:20:43 +0500

    add select->library-song

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,12 +1,12 @@
 import LibrarySong from "./LibrarySong";
 
-const Library = ({songs}) => {
+const Library = ({songs,setCurrentSong}) => {
   return (
     <div className="library">
       <h2>Library</h2>
       <div className="library-songs">
         {songs.map((song,index) => (
-            <LibrarySong song={song} key={index} />
+            <LibrarySong song={song} key={song.id} setCurrentSong={setCurrentSong} />
         ))}
       </div>
     </div>
